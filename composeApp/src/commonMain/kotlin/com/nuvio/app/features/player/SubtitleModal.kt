daf229f2da77d5118d5d5963cc72a28f1e57f2cc
MCNGN <rifqi.adibbahagia@binus.ac.id>
2026-08-12 08:57:01 +0000

    fix(player): import missing material3 Surface for tooltip

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/SubtitleModal.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/SubtitleModal.kt
@@ -30,6 +30,7 @@ import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.CloudDownload
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
